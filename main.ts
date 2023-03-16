2954d1205200b5b76f33cfaa59482fc645fed365
zoofred61 <128042375+zoofred61@users.noreply.github.com>
2023-03-16 14:56:46 +0100

    Mise à jour pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,21 +1,27 @@
-input.onSound(DetectedSound.Loud, function () {
-    basic.showLeds(`
-        . # . # .
-        . # . # .
-        . . . . .
-        . # # # .
-        # . . . #
-        `)
+input.onButtonPressed(Button.A, function () {
+    music.playSoundEffect(music.createSoundEffect(
+    WaveShape.Sine,
+    2786,
+    1,
+    255,
+    0,
+    1049,
+    SoundExpressionEffect.None,
+    InterpolationCurve.Linear
+    ), SoundExpressionPlayMode.UntilDone)
 })
-input.onSound(DetectedSound.Quiet, function () {
-    basic.showLeds(`
-        . # . # .
-        . # . # .
-        . . . . .
-        # . . . #
-        . # # # .
-        `)
+input.onButtonPressed(Button.B, function () {
+    music.playSoundEffect(music.createSoundEffect(
+    WaveShape.Sine,
+    1,
+    2786,
+    255,
+    0,
+    1049,
+    SoundExpressionEffect.None,
+    InterpolationCurve.Linear
+    ), SoundExpressionPlayMode.UntilDone)
 })
 basic.forever(function () {
-    input.setSoundThreshold(SoundThreshold.Loud, 25)
+    input.setSoundThreshold(SoundThreshold.Loud, 128)
 })
