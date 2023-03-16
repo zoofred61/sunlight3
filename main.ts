8a2fe6f52364f317b70c6b5204253066760cb79a
zoofred61 <128042375+zoofred61@users.noreply.github.com>
2023-03-16 14:48:33 +0100

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,21 @@
+input.onSound(DetectedSound.Loud, function () {
+    basic.showLeds(`
+        . # . # .
+        . # . # .
+        . . . . .
+        . # # # .
+        # . . . #
+        `)
+})
+input.onSound(DetectedSound.Quiet, function () {
+    basic.showLeds(`
+        . # . # .
+        . # . # .
+        . . . . .
+        # . . . #
+        . # # # .
+        `)
+})
 basic.forever(function () {
-    if (input.lightLevel() > 100) {
-        music.playTone(262, music.beat(BeatFraction.Sixteenth))
-    } else {
-        music.playTone(523, music.beat(BeatFraction.Sixteenth))
-    }
+    input.setSoundThreshold(SoundThreshold.Loud, 25)
 })
